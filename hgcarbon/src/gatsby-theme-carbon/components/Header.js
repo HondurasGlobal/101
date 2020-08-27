98e1cb9d0eb04d5835e03469f06b657eea9b321c
Stanley Marrder <eniver@mac.com>
2020-08-27 06:50:03 -0500

    Changed header

--- hgcarbon/src/gatsby-theme-carbon/components/Header.js
+++ hgcarbon/src/gatsby-theme-carbon/components/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import Header from 'gatsby-theme-carbon/src/components/Header';
+/** @format */
+
+import React from "react";
+import Header from "gatsby-theme-carbon/src/components/Header";
 
 const CustomHeader = (props) => (
-  <Header {...props}>
-    Gatsby theme&nbsp;<span>Carbon</span>
-  </Header>
+	<Header {...props}>
+		Honduras &nbsp;<span>Global</span>
+	</Header>
 );
 
 export default CustomHeader;
